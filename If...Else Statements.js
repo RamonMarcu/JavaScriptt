f9b5e5af3e2c3ddd290df124d9167e3e0044e979
Ramon Marcu <70721540+RamonMarcu@users.noreply.github.com>
2020-11-27 18:03:50 +0200

    Update and rename js.js to If...Else Statements.js

---
+++ If...Else Statements.js
@@ -0,0 +1,23 @@
+let sale = true;
+
+sale = false;
+
+if(true) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+
+/* If run the first example console displays: Time to buy! */
+
+let sale = true;
+
+sale = false;
+
+if(false) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+
+/* If run the second example console displays: Time to wait for a sale */
